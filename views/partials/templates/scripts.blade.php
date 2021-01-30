eab66a415ed85f49a2fe57e9c0c7214e9e3781cd
ruslan_saiko <rus.sayko17@gmail.com>
2021-01-30 18:09:55 +0200

    Add (all files) Добавил шаблон

---
+++ views/partials/templates/scripts.blade.php
@@ -0,0 +1,16 @@
+<script src="assets/templates/js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+<script src="assets/templates/vendor/wow/wow.js"></script><!-- WOW JS -->
+<script src="assets/templates/vendor/bootstrap/js/popper.min.js"></script><!-- POPPER.MIN JS -->
+<script src="assets/templates/vendor/bootstrap/js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+<script src="assets/templates/vendor/owl-carousel/owl.carousel.js"></script><!-- OWL-CAROUSEL JS -->
+<script src="assets/templates/vendor/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC-POPUP JS -->
+<script src="assets/templates/vendor/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+<script src="assets/templates/vendor/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+<script src="assets/templates/vendor/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+<script src="assets/templates/vendor/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+<script src="assets/templates/vendor/masonry/masonry.filter.js"></script><!-- MASONRY -->
+<script src="assets/templates/vendor/lightgallery/js/lightgallery-all.min.js"></script><!-- LIGHTGALLERY -->
+<script src="assets/templates/vendor/bootstrap-select/bootstrap-select.min.js"></script><!-- BOOTSTRAP SELECT -->
+<script src="assets/templates/js/dz.carousel.js"></script><!-- OWL-CAROUSEL -->
+<script src="assets/templates/js/dz.ajax.js"></script><!-- CONTACT JS  -->
+<script src="assets/templates/js/custom.js"></script><!-- CUSTOM JS -->
