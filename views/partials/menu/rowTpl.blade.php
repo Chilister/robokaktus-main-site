3760d518d61fd28a7cea0885c75896996166ad97
ruslan_saiko <rus.sayko17@gmail.com>
2021-02-13 15:48:11 +0200

    Change (header.blade.php, outerTpl.blade.php, rowTpl.blade.php) Поправил header и сделал меню через DlMenu

---
+++ views/partials/menu/rowTpl.blade.php
@@ -0,0 +1,5 @@
+<li>
+    <a href="{{ $data['url'] }}" title="{{ $data['title'] }}">
+        <span>{{ $data['title'] }}</span>
+    </a>
+</li>
--- views/partials/templates/header.blade.php
+++ views/partials/templates/header.blade.php
@@ -1,21 +1,32 @@
 <header class="site-header mo-left header-transparent">
-    <div class="container">
-        <div class="top-bar py-0">
-            <div class="row d-flex justify-content-between align-items-center">
-                <div class="dlab-topbar-left d-flex align-items-center">
-                    <div class="logo-header mostion">
-                        <a href="#"><img class="logo-2 w-50" src="logo.png" alt=""></a>
-                    </div>
-                    <ul>
-                        <li><a class="text-white" href="mailto:robokaktus.web@gmail.com"><i class="la la-envelope"></i>robokaktus.web@gmail.com</a></li>
-                    </ul>
+    <div class="sticky-header main-bar-wraper navbar-expand-lg is-fixed">
+        <div class="main-bar clearfix ">
+            <div class="container clearfix">
+                <div class="logo-header mostion">
+                    <a href="/"><img class="logo-2 w-50" src="/logo.png" alt="logo"></a>
                 </div>
-                <div class="dlab-topbar-right">
-                    <div class="extra-nav">
-                        <div class="extra-cell">
-                            <a href="#contact-form" class="btn btn-light rounded-xl shadow">Замовити сайт</a>
-                        </div>
+                <button class="navbar-toggler collapsed navicon justify-content-end" type="button"
+                        data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div class="extra-nav">
+                    <div class="extra-cell">
+                        <a href="#contact-form" title="Замовити сайт" class="btn btn-light contact-form rounded-xl shadow">Замовити сайт</a>
                     </div>
+                </div>
+                <div class="header-nav navbar-collapse collapse justify-content-end" id="navbarNavDropdown">
+                    <div class="logo-header">
+                        <a href="index.html"><img src="/logo.png" alt="logo"></a>
+                    </div>
+                    {!! $modx->runSnippet('DLMenu',[
+                        'parents'=>0,
+                        'outerTpl'=>'@B_FILE: partials/menu/outerTpl',
+                        'rowTpl'=>'@B_FILE: partials/menu/rowTpl',
+                        'maxDepth'=>2
+                    ]) !!}
                 </div>
             </div>
         </div>
